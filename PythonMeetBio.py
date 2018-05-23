4e71daa9a6d2eebfe9a824094ae38d2f644c9d5d
malcogene <malcogene@malcogeneui-iMac.local>
2018-05-23 17:15:26 +0900

    20180523 modified
    Motifs.py

--- PythonMeetBio.py
+++ PythonMeetBio.py
@@ -307,3 +307,28 @@ def ApproximatePatternCount(Pattern, Text, d):
 
 # def ApproximatePatternCount(Pattern, Text, d):
 #     return len(ApproximatePatternMatching(Text, Pattern, d))
+
+#20180523 "Motifs.py"
+# Input:  A set of kmers Motifs
+# Output: Count(Motifs)
+def Count(Motifs):
+    count = {} # initializing the count dictionary
+    k = len(Motifs[0])
+    for symbol in "ACGT":
+        count[symbol] = []
+        for j in range(k):
+             count[symbol].append(0)
+    t = len(Motifs)
+    for i in range(t):
+        for j in range(k):
+            symbol = Motifs[i][j]
+            count[symbol][j] += 1
+    return count
+# Input:  A list of kmers Motifs
+# Output: the profile matrix of Motifs, as a dictionary of lists.
+def Profile(Motifs):
+    t = len(Motifs)
+    k = len(Motifs[0])
+    profile = {}
+    # insert your code here
+    return profile
